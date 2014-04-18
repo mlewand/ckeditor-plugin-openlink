ae92f3a4d28e61d6ac9d401a561d8aad10b92467
m.lewandowski <m.lewandowski@cksource.com>
2014-04-18 14:57:55 +0200

    Changed method to attach listener to editable and updated ctrl handling because it's already natively handled by FF.

--- plugin.js
+++ plugin.js
@@ -60,21 +60,26 @@
 			} );
 
 			// A quick workaround for issue #11842.
-			editor.on( 'instanceReady', function( evt ) {
+			editor.on( 'contentDom', function( evt ) {
 				var editable = editor.editable();
 
 				// We want to be able to open links also in read-only mode. This
 				// listener will open link in new tab.
-				editable.on( 'click', function( evt ) {
+				editable.attachListener( editable, 'click', function( evt ) {
 					// This feature should be available in:
 					// * wysywigmode in read-only
 					// * wysywigmode when ctrl key is down
-					if ( !editor.readOnly && !evt.data.$.ctrlKey )
+
+					// FF handles it by itself. Var is inited few lines below.
+					ctrlClickHandlingNeeded = evt.data.$.ctrlKey && !CKEDITOR.env.gecko;
+
+					if ( !editor.readOnly && !ctrlClickHandlingNeeded )
 						return;
 
 					var target = evt.data.getTarget(),
 						clickedAnchor = ( new CKEDITOR.dom.elementPath( target, editor.editable() ) ).contains( 'a' ),
-						href = clickedAnchor && clickedAnchor.getAttribute( 'href' );
+						href = clickedAnchor && clickedAnchor.getAttribute( 'href' ),
+						ctrlClickHandlingNeeded;
 
 					if ( href )
 						window.open( href, '_blank' );
